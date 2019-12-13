e8d0a0d62b981c0e167d030a187f83ec060e4dd5
chenwentong <609042866@qq.com>
2019-12-13 17:35:17 +0800

    更新代碼

--- src/main/java/JSUtil.java
+++ src/main/java/JSUtil.java
@@ -1,52 +1,18 @@
-import jdk.internal.org.xml.sax.SAXException;
 
-import java.io.*;
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class JSUtil
 {
+    protected static LinkedBlockingQueue<String> produceQueue = new LinkedBlockingQueue<String>();
+    protected static Set<String> cosumeSet = new HashSet<String>();
 
-    // 如果要更换运行环境，请注意exePath最后的phantom.exe需要更改。因为这个只能在window版本上运行。前面的路径名
-    // 也需要和exePath里面的保持一致。否则无法调用
-    private static String projectPath = System.getProperty("user.dir");
-    private static String jsPath = projectPath + File.separator + "crawl.js";
-    private static String exePath = "E:" + File.separator + "phantomjs" + File.separator + "bin" + File.separator
-            + "phantomjs.exe";
-    private static String REGEX_CHINESE = "[^\u4e00-\u9fa5]";// 中文正则
-    private LinkedBlockingQueue<String> produceQueue = new LinkedBlockingQueue<String>();
-    private Set<String> cosumeSet = new HashSet<String>();
+    public static void main(String[] args) throws InterruptedException {
 
-    public static void main(String[] args) throws IOException, SAXException
-    {
-
+        produceQueue.put("http://www-dev.ctyun.cn/home/");
         new Thread(new ProduceUrl()).start();
         new Thread(new ConsumeUrl()).start();
-        System.out.println(projectPath);
-        // 测试调用。传入url即可
-        String html = getParseredHtml2("http://www-dev.ctyun.cn/home/");
-        System.out.println("html: " + html);
-    }
-
-    // 调用phantomjs程序，并传入js文件，并通过流拿回需要的数据。
-    public static String getParseredHtml2(String url) throws IOException
-    {
-        //取得当前JVM的运行时环境,Singleton的
-        Runtime rt = Runtime.getRuntime();
-        Process p = rt.exec(exePath + " " + jsPath + " " + url);
-        InputStream is = p.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        StringBuffer sbf = new StringBuffer();
-        String tmp = "";
-        while ((tmp = br.readLine()) != null)
-        {
-            sbf.append(tmp);
-        }
-        System.out.println(sbf.toString());
-        return sbf.toString();
     }
 
 }
--- src/main/java/ProduceUrl.java
+++ src/main/java/ProduceUrl.java
@@ -1,17 +1,46 @@
-import java.io.File;
+import java.io.*;
 
 public class ProduceUrl implements Runnable{
     // 如果要更换运行环境，请注意exePath最后的phantom.exe需要更改。因为这个只能在window版本上运行。前面的路径名
     // 也需要和exePath里面的保持一致。否则无法调用
-    private static String projectPath = System.getProperty("user.dir");
-    private static String jsPath = projectPath + File.separator + "crawl.js";
-    private static String exePath = "E:" + File.separator + "phantomjs" + File.separator + "bin" + File.separator
+    protected static String projectPath = System.getProperty("user.dir");
+    protected static String jsPath = projectPath + File.separator + "crawl.js";
+    protected static String exePath = "E:" + File.separator + "phantomjs" + File.separator + "bin" + File.separator
             + "phantomjs.exe";
+    protected static String REGEX_CHINESE = "[^\u4e00-\u9fa5]";// 中文正则
 
     public ProduceUrl(){
 
     }
     public void run() {
+        while(!JSUtil.produceQueue.isEmpty()) {
+            try {
+                String url = JSUtil.produceQueue.take();
+                getParseredHtml2(url);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+
+    // 调用phantomjs程序，并传入js文件，并通过流拿回需要的数据。
+    public static String getParseredHtml2(String url) throws IOException
+    {
+        //取得当前JVM的运行时环境,Singleton的
+        Runtime rt = Runtime.getRuntime();
+        Process p = rt.exec(exePath + " " + jsPath + " " + url);
+        InputStream is = p.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuffer sbf = new StringBuffer();
+        String tmp = "";
+        while ((tmp = br.readLine()) != null)
+        {
+            sbf.append(tmp);
+        }
+        System.out.println(sbf.toString());
+        return sbf.toString();
     }
 }
